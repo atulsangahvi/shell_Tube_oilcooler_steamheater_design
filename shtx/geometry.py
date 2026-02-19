529851aab4a0cdf3b5e6a4af3fa98927fe0a54b2
atulsangahvi <61201853+atulsangahvi@users.noreply.github.com>
2026-02-19 11:30:29 +0400

    Add files via upload

---
+++ shtx/geometry.py
@@ -0,0 +1,15 @@
+import math
+
+def tube_outside_area(n, do, L):
+    return n * math.pi * do * L
+
+def tube_flow_area(n, di, passes):
+    tpp = max(n / max(passes, 1), 1.0)
+    return tpp * math.pi * (di**2) / 4
+
+def approx_shell_crossflow_area(Ds, bs, bc):
+    # very simple crossflow area estimate, used for velocity scaling
+    return Ds * bs * max(1 - bc, 0.2)
+
+def tube_pitch(do_m: float, p_over_do: float):
+    return do_m * p_over_do
